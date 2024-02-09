d82c22195798236332f478de63d607d33b458230
Crowdbotics <engineering@crowdbotics.com>
2024-02-09 18:26:39 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "calm-forest-dev-133172",
+  url: "https://calm-forest-dev-133172.botics.co",
+  api: "https://calm-forest-dev-133172.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
